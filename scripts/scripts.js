a523cba8424808472871187aff58fab247b7b527
woland-szr <62295565+woland-szr@users.noreply.github.com>
2020-05-11 14:47:44 +0400

    slider v1.1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,14 @@
-let Slider = {
+let slider = {
     curSlide: 1,
-    City: ['Rostov-on-Don<br>LCD admiral', 'Sochi<br>Thieves', 'Rostov-on-Don<br>Patriotic'],
-    Area: [81, 105, 93],
-    Time: [3.5, 4, 3],
+    city: ['Rostov-on-Don<br>LCD admiral', 'Sochi<br>Thieves', 'Rostov-on-Don<br>Patriotic'],
+    area: [81, 105, 93],
+    time: [3.5, 4, 3],
     showSlide() {
         for (let i = 1; i <= 3; i++) {
-            let activeLi = document.querySelector('.main__block-one__menu__li[data-slide="'+i+'"]');
-            let activeDot = document.querySelector('.main__block-one__slider__dot[data-slide="'+i+'"]');
+//            let activeLi = document.querySelector('.main__block-one__menu__li[data-slide="'+i+'"]');
+//            let activeDot = document.querySelector('.main__block-one__slider__dot[data-slide="'+i+'"]');
+            let activeLi = topLinks[i-1];
+            let activeDot = dotLinks[i-1];
             if (i == this.curSlide) {
                 activeLi.classList.add('li_active');
                 activeDot.classList.add('dot_active');
@@ -16,28 +18,34 @@ let Slider = {
                 activeDot.classList.remove('dot_active');
             }
         }
-        document.getElementById('city-left').innerHTML = this.City[this.curSlide-1];
-        document.getElementById('area-left').innerHTML = this.Area[this.curSlide-1];
-        document.getElementById('time-left').innerHTML = this.Time[this.curSlide-1];
-        document.getElementById('slide-image').src = `./images/image${this.curSlide}.jpg`;
+        cityContent.innerHTML = this.city[this.curSlide-1];
+        areaContent.innerHTML = this.area[this.curSlide-1];
+        timeContent.innerHTML = this.time[this.curSlide-1];
+        imgContent.src = `./images/image${this.curSlide}.jpg`;
     }    
 }
 
+let cityContent = document.getElementById('city-left');
+let areaContent = document.getElementById('area-left');
+let timeContent = document.getElementById('time-left');
+let imgContent = document.getElementById('slide-image');
+let topLinks = document.getElementsByClassName('main__block-one__menu__li')
+let dotLinks = document.getElementsByClassName('main__block-one__slider__dot')
+let arrowLinks = document.getElementsByClassName('slider-arrows')
+
 const chooseSlide = (event) => {
-    Slider.curSlide = event.target.dataset.slide;
+    slider.curSlide = event.target.dataset.slide;
     clearInterval(intervalID);
-    Slider.showSlide();
+    slider.showSlide();
 }
 
-Slider.showSlide();
+slider.showSlide();
 
-let intervalID = setInterval (() => {
-    Slider.curSlide++;
-    if (Slider.curSlide > 3) Slider.curSlide = 1;
-    Slider.showSlide();
-}, 3000);
-
-let topLinks = document.getElementsByClassName('main__block-one__menu__li')
+  let intervalID = setInterval (() => {
+      slider.curSlide++;
+      if (slider.curSlide > 3) slider.curSlide = 1;
+      slider.showSlide();
+  }, 3000);
 
 for (let link of topLinks) {
     link.addEventListener('click', (event) => {
@@ -45,26 +53,22 @@ for (let link of topLinks) {
     });
 }
 
-let dotLinks = document.getElementsByClassName('main__block-one__slider__dot')
-
 for (let link of dotLinks) {
     link.addEventListener('click', (event) => {
         chooseSlide(event);
     });
 }
 
-let arrowLinks = document.getElementsByClassName('slider-arrows')
-
 for (let link of arrowLinks) {
     link.addEventListener('click', (event) => {
         clearInterval(intervalID);
         if (event.target.id === 'next-arrow') {
-            Slider.curSlide++;
-            if (Slider.curSlide > 3) Slider.curSlide = 1;
+            slider.curSlide++;
+            if (slider.curSlide > 3) slider.curSlide = 1;
         } else {
-            Slider.curSlide--;
-            if (Slider.curSlide < 1) Slider.curSlide = 3;
+            slider.curSlide--;
+            if (slider.curSlide < 1) slider.curSlide = 3;
         } 
-    Slider.showSlide();
+    slider.showSlide();
     });
 }
