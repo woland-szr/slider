af80d6521ae5e3fcd3dd955edef2c7236afdd37c
woland-szr <62295565+woland-szr@users.noreply.github.com>
2020-05-13 14:20:56 +0400

    slider v1.3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,49 +2,48 @@ let slider = {
     curSlide: 1,
     city: ['Rostov-on-Don<br>LCD admiral', 'Sochi<br>Thieves', 'Rostov-on-Don<br>Patriotic'],
     area: [81, 105, 93],
-    time: [3.5, 4, 3],
-    showSlide() {
-        for (let i = 1; i <= 3; i++) {
-//            let activeLi = document.querySelector('.main__block-one__menu__li[data-slide="'+i+'"]');
-//            let activeDot = document.querySelector('.main__block-one__slider__dot[data-slide="'+i+'"]');
-            const activeLi = topLinks[i-1];
-            const activeDot = dotLinks[i-1];
-            if (i == this.curSlide) {
-                activeLi.classList.add('li_active');
-                activeDot.classList.add('dot_active');
-            }
-            else {
-                activeLi.classList.remove('li_active');
-                activeDot.classList.remove('dot_active');
-            }
-        }
-        cityContent.innerHTML = this.city[this.curSlide-1];
-        areaContent.innerHTML = this.area[this.curSlide-1];
-        timeContent.innerHTML = this.time[this.curSlide-1];
-        imgContent.src = `./images/image${this.curSlide}.jpg`;
-    }    
+    time: [3.5, 4, 3]
 }
 
 const cityContent = document.getElementById('city-left');
 const areaContent = document.getElementById('area-left');
 const timeContent = document.getElementById('time-left');
 const imgContent = document.getElementById('slide-image');
-const topLinks = document.getElementsByClassName('main__block-one__menu__li')
-const dotLinks = document.getElementsByClassName('main__block-one__slider__dot')
-const arrowLinks = document.getElementsByClassName('slider-arrows')
+const topLinks = document.getElementsByClassName('main__block-one__menu__li');
+const dotLinks = document.getElementsByClassName('main__block-one__slider__dot');
+const arrowLinks = document.getElementsByClassName('slider-arrows');
+
+function showSlide() {
+    for (let i = 1; i <= 3; i++) {
+        const activeLi = topLinks[i-1];
+        const activeDot = dotLinks[i-1];
+        if (i == slider.curSlide) {
+            activeLi.classList.add('li_active');
+            activeDot.classList.add('dot_active');
+        }
+        else {
+            activeLi.classList.remove('li_active');
+            activeDot.classList.remove('dot_active');
+        }
+    }
+    cityContent.innerHTML = slider.city[slider.curSlide-1];
+    areaContent.innerHTML = slider.area[slider.curSlide-1];
+    timeContent.innerHTML = slider.time[slider.curSlide-1];
+    imgContent.src = `./images/image${slider.curSlide}.jpg`;
+}  
 
 const chooseSlide = (event) => {
     slider.curSlide = event.target.dataset.slide;
     clearInterval(intervalID);
-    slider.showSlide();
+    showSlide();
 }
 
-slider.showSlide();
+showSlide();
 
   let intervalID = setInterval (() => {
       slider.curSlide++;
       if (slider.curSlide > 3) slider.curSlide = 1;
-      slider.showSlide();
+      showSlide();
   }, 3000);
 
 for (let link of topLinks) {
@@ -69,6 +68,6 @@ for (let link of arrowLinks) {
             slider.curSlide--;
             if (slider.curSlide < 1) slider.curSlide = 3;
         } 
-    slider.showSlide();
+    showSlide();
     });
 }
